8da5dd5bdd806ba50d109967827b83c586448bcb
jchwenger <34098722+jchwenger@users.noreply.github.com>
2020-04-13 18:52:45 +0100

    encode.py blackened, adding option for sentencepiece encoder/decoder

--- encode.py
+++ encode.py
@@ -7,25 +7,68 @@ import argparse
 import numpy as np
 
 import encoder
+import encoder_sp as encoder_sp
 from load_dataset import load_dataset
 
 parser = argparse.ArgumentParser(
-    description='Pre-encode text files into tokenized training set.',
-    formatter_class=argparse.ArgumentDefaultsHelpFormatter)
-parser.add_argument('--model_name', metavar='MODEL', type=str, default='117M', help='Pretrained model name')
-parser.add_argument('--combine', metavar='CHARS', type=int, default=50000, help='Concatenate files with <|endoftext|> separator into chunks of this minimum size')
-parser.add_argument('--encoding', type=str, default='utf-8', help='Set the encoding for reading and writing files.')
-parser.add_argument('in_text', metavar='PATH', type=str, help='Input file, directory, or glob pattern (utf-8 text).')
-parser.add_argument('out_npz', metavar='OUT.npz', type=str, help='Output file path')
+    description="Pre-encode text files into tokenized training set.",
+    formatter_class=argparse.ArgumentDefaultsHelpFormatter,
+)
+parser.add_argument(
+    "--model_name",
+    metavar="MODEL",
+    type=str,
+    default="1558M",
+    help="Pretrained model name",
+)
+parser.add_argument(
+    "--combine",
+    metavar="CHARS",
+    type=int,
+    default=50000,
+    help="Concatenate files with <|endoftext|> separator into chunks of this minimum size",
+)
+parser.add_argument(
+    "--encoding",
+    type=str,
+    default="utf-8",
+    help="Set the encoding for reading and writing files.",
+)
+parser.add_argument(
+    "in_text",
+    metavar="PATH",
+    type=str,
+    help="Input file, directory, or glob pattern (utf-8 text).",
+)
+
+parser.add_argument(
+    "out_npz",
+    metavar="OUT.npz",
+    type=str,
+    help="Output file path"
+)
+
+parser.add_argument(
+    "encoder",
+    metavar="ENCODER",
+    choices=["default", "sentencepiece"],
+    default="default",
+    type=str,
+    help="Type of encoder. Choices: default, sentencepiece. Default: default provided by OpenAI, src/encoder.py",
+)
+
 
 def main():
     args = parser.parse_args()
-    enc = encoder.get_encoder(args.model_name)
-    print('Reading files')
+    if args.encoder == "default":
+        enc = encoder.get_encoder(args.model_name)
+    elif args.encoder == "sentencepiece":
+        enc = encoder_sp.get_encoder("models", args.model_name)
+    print("Reading files")
     chunks = load_dataset(enc, args.in_text, args.combine, encoding=args.encoding)
-    print('Writing', args.out_npz)
+    print("Writing", args.out_npz)
     np.savez_compressed(args.out_npz, *chunks)
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
