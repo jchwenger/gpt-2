bcc8f0c2f95211e9b4bc76ce76a97a0e141e2682
jchwenger <34098722+jchwenger@users.noreply.github.com>
2020-04-13 18:51:00 +0100

    importing encoder_sp (sentencepiece) from rkfg-scratch, blackened

---
+++ src/encoder_sp.py
@@ -0,0 +1,19 @@
+"""Byte pair encoding utilities"""
+import os
+import sentencepiece as spm
+
+
+class Encoder:
+    def __init__(self, filename):
+        self.sp = spm.SentencePieceProcessor()
+        self.sp.Load(filename)
+
+    def encode(self, text):
+        return self.sp.EncodeAsIds(text)
+
+    def decode(self, tokens):
+        return self.sp.DecodeIds(tokens.tolist()).replace("<|n|>", "\n")
+
+
+def get_encoder(models_dir, model_name):
+    return Encoder(os.path.join(models_dir, model_name, "sp.model"))
