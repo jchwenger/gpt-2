1b2c25bbac0994e3c20dcb98d664bec238b27d5b
jchwenger <34098722+jchwenger@users.noreply.github.com>
2020-08-20 14:27:16 +0100

    encoder hug | handling special tokens

--- src/encoder_hug.py
+++ src/encoder_hug.py
@@ -4,11 +4,17 @@ from tokenizers import ByteLevelBPETokenizer
 
 
 class Encoder:
-    def __init__(self, dirname):
+    def __init__(self, dirname, special_tokens=["<|endoftext|>"]):
         self.tok = ByteLevelBPETokenizer(
-        vocab_file=os.path.join(dirname, "encoder.json"),
-        merges_file=os.path.join(dirname, "vocab.bpe"),
+            vocab_file=os.path.join(dirname, "encoder.json"),
+            merges_file=os.path.join(dirname, "vocab.bpe"),
         )
+        if special_tokens:
+            for s in special_tokens:
+                assert (
+                    s in self.tok.get_vocab()
+                ), f"Special token '{s}' not found in encoder!"
+            self.tok.add_special_tokens(special_tokens)
 
     def encode(self, text):
         return self.tok.encode(text).ids
@@ -17,5 +23,10 @@ class Encoder:
         return self.tok.decode(tokens)
 
 
-def get_encoder(model_name, models_dir="models"):
-    return Encoder(os.path.join(models_dir, model_name))
+def get_encoder(model_name, models_dir="models", special_tokens=None):
+    if special_tokens is not None:
+        return Encoder(
+            os.path.join(models_dir, model_name), special_tokens=special_tokens
+        )
+    else:
+        return Encoder(os.path.join(models_dir, model_name,))
